743eda394811b292e59c32f5220896d6e5d65ef8
mrjasonnlin <mrjasonnlin@gmail.com>
2024-07-02 12:06:57 +0800

    P1

--- render/views.py
+++ render/views.py
@@ -5,19 +5,3 @@ from django.shortcuts import render
 def index(request):
     context = {'like': 'Django 很棒'}
     return render(request, 'render/index.html', context)
-
-
-#def bike(request):
-    """
-    Render the bike page
-    """
-#    return render(request, 'main/bike.html')
-
-
-#def main(request):
-    """
-    Render the main page
-    """
-#    context = {'like': 'Django 很棒 render view'}
-#    return render(request, 'main/main.html', context)
-
